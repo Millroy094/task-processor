6593b7a65dc2324801c88a78c46271db6ba753b6
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-11-22 11:42:19 +0000

    chore: started creating consumer producer type atchitechure

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/millroy094/task-processor
 
 go 1.22.3
+
+require github.com/streadway/amqp v1.1.0 // indirect
--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import ("encoding/json", "net/http", "log")
-
-type Task struct {
-	ID int `json:"id"`
-	Type int `json:"type"`
-	Payload int `json:"payload"`
-}
