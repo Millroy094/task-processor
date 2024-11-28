61edee497f44b6b5f77dd02f344f7af0dc872d4d
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-11-28 15:16:00 +0000

    chore: improvements and tweaks

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/signal"
 	"runtime"
+	"sync"
 	"syscall"
 
 	"github.com/millroy094/task-processor/pkg/common"
@@ -20,27 +21,41 @@ func performTask(task task.Task) error {
 	return nil
 }
 
-func processor(id int, messages <-chan amqp.Delivery) {
-	for message := range messages {
-		var task task.Task
-		err := json.Unmarshal(message.Body, &task)
+func processor(id int, messages <-chan amqp.Delivery, shutdownChan <-chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
 
-		if err != nil {
-			log.Printf("Worker %d: Failed to deserialize task: %v\n", id, err)
-			message.Nack(false, false)
-			continue
+	for {
+		select {
+		case message, ok := <-messages:
+			if !ok {
+				log.Printf("Worker %d: Stopping\n", id)
+				return
+			}
+
+			var task task.Task
+			err := json.Unmarshal(message.Body, &task)
+
+			if err != nil {
+				log.Printf("Worker %d: Failed to deserialize task: %v\n", id, err)
+				message.Nack(false, false)
+				continue
+			}
+
+			if err := performTask(task); err != nil {
+				log.Printf("Worker %d: Task failed: %v\n", id, err)
+				message.Nack(false, true)
+				continue
+			}
+
+			fmt.Printf("Worker %d processing task ID %d of type %s\n", id, task.ID, task.Type)
+
+			message.Ack(false)
+		case <-shutdownChan:
+			log.Printf("Worker %d: Received shutdown signal\n", id)
+			return
 		}
-
-		if err := performTask(task); err != nil {
-			log.Printf("Worker %d: Task failed: %v\n", id, err)
-			message.Nack(false, true)
-			continue
-		}
-
-		fmt.Printf("Worker %d processing task ID %d of type %s\n", id, task.ID, task.Type)
-
-		message.Ack(false)
 	}
+
 }
 
 func main() {
@@ -64,12 +79,20 @@ func main() {
 
 	common.FailOnError(err, "Failed to register a consumer")
 
-	numWorkers := runtime.NumCPU()
-	for i := 1; i <= numWorkers; i++ {
-		go processor(i, messages)
-	}
-
 	stopChan := make(chan os.Signal, 1)
 	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM)
+
+	shutdownChan := make(chan struct{})
+	var wg sync.WaitGroup
+
+	numWorkers := runtime.NumCPU()
+	for i := 1; i <= numWorkers; i++ {
+		go processor(i, messages, shutdownChan, &wg)
+	}
+
 	<-stopChan
+	log.Println("Shutdown signal received, stopping workers...")
+
+	close(shutdownChan)
+
 }
