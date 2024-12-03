deea1192153179f6831bef4ef63294941bcc79c9
Millroy Fernandes <132992164+mfernandes-alcumus@users.noreply.github.com>
2024-12-03 14:01:01 +0000

    tweaks

--- pkg/task/task.go
+++ pkg/task/task.go
@@ -20,13 +20,13 @@ type TaskResult struct {
 	Timestamp time.Time `json:"timestamp"`
 }
 
-// @Description Represents a task with ID, type, and payload information.
+// @Description Represents a task with type, and payload information.
 type Task struct {
 	// @ID Task ID
 	// @example 1
-	// It will only appear when retrieving the task (GET or PUT).
+	// It will only appear when retrieving the task (GET).
 	// @ReadOnly
-	ID int `json:"id"`
+	ID int `json:"id,omitempty" swaggerignore:"true"`
 
 	// @Type Type of the task (e.g., "email", "health check")
 	// @example "email"
@@ -34,31 +34,32 @@ type Task struct {
 
 	// @Payload The actual task payload (which can vary depending on the task type)
 	// Using RawMessage to delay unmarshalling
+	// @oneOf {emailPayload} {healthCheckPayload}
 	Payload json.RawMessage `json:"payload"`
 
 	// @CreatedAt The time the task was created
 	// @example "2024-11-27T12:30:00Z"
-	// It will only appear when retrieving the task (GET or PUT).
+	// It will only appear when retrieving the task (GET).
 	// @ReadOnly
-	CreatedAt time.Time `json:"createdAt"`
+	CreatedAt time.Time `json:"createdAt,omitempty" swaggerignore:"true"`
 
 	// @Status The current status of the task (e.g., "pending", "in-progress", "completed")
 	// @example "pending"
-	// This field will be auto-populated on the server-side, hence not included in the body when creating a task.
-	// It will only appear when retrieving the task (GET or PUT).
+	// This field will be auto-populated on the server-side.
+	// It will only appear when retrieving the task (GET).
 	// @ReadOnly
-	Status string `json:"status"`
+	Status string `json:"status,omitempty" swaggerignore:"true"`
 
 	// @FinishedAt The time when the task was finished (only updated when task is completed)
 	// @example "2024-11-27T12:35:00Z"
-	// This field will be auto-populated on the server-side, hence not included in the body when creating a task.
-	// It will only appear when retrieving the task (GET or PUT).
+	// This field will be auto-populated on the server-side.
+	// It will only appear when retrieving the task (GET).
 	// @ReadOnly
-	FinishedAt time.Time `json:"finishedAt,omitempty"`
+	FinishedAt time.Time `json:"finishedAt,omitempty" swaggerignore:"true"`
 
 	// @Result The result of the task, includes status, error, and detail.
 	// This field will be auto-populated by the backend after task completion.
 	// @example {"status": "completed", "timestamp": "2024-12-03T12:35:00Z"}
 	// @ReadOnly
-	Result TaskResult `json:"result,omitempty"` // This field will be set by the server, not the client.
+	Result TaskResult `json:"result,omitempty" swaggerignore:"true"`
 }
